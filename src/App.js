d89a130605e5cec389824a876c3271f957a89e2f
Nithin John Cherian <Nithinjohn2811"gmail.com>
2024-02-14 11:32:09 +0000

    toggle and delete created,map and filter used

--- src/App.js
+++ src/App.js
@@ -2,52 +2,75 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
- //2 //2 end
-  const [newItem, setnewItem] = useState()
+  //2 //2 end
+  const [newItem, setnewItem] = useState();
+  //4
+  const [todos, setTodos] = useState([]);
 
-  //4 
+  function handleSubmit(e) {
+    e.preventDefault();
+    //anytime we want to pass acurrent value,pass a function or else pass a value like e.target.value
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
 
-  function handleSubmit(e){
-    e.preventDefault()
+    setnewItem("");
   }
   //4end
+
+  // 6 .onchange on toggle and onclick on delete also part of this toggletodo
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed } : todo
+      );
+    });
+  }
+
+  function deleteTodo(id, completed) {
+    setTodos((currentTodos) => currentTodos.filter((todo) => todo.id !== id));
+  }
+
+  //6 end
 
   return (
     <div className="App">
       {/*1 */}
       <form onSubmit={handleSubmit}>
         <label htmlFor="item">New Item</label>
-         {/*3  value and onChange*/}
-        <input  value ={newItem} onChange = {e=>setnewItem(e.target.value)}type="text" id="item" />
-         {/*3 end */}
+        {/*3  value and onChange*/}
+        <input
+          value={newItem}
+          onChange={(e) => setnewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+        {/*3 end */}
         <button className="btn">click</button>
       </form>
       <h1>Todo list</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button>Delete</button>
-        </li>
+        {/*5 */}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button>Delete</button>
+            </li>
+          );
+        })}
 
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button>Delete</button>
-        </li>
-
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 3
-          </label>
-          <button>Delete</button>
-        </li>
-
+        {/*5 end */}
       </ul>
 
       {/*1 end */}
