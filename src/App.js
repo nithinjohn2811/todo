b1362303b6a5c7a14925e85ac4eae7b5b15d0420
Nithin John Cherian <Nithinjohn2811"gmail.com>
2024-02-14 09:47:21 +0000

    onchange,handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+ //2 //2 end
+  const [newItem, setnewItem] = useState()
+
+  //4 
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
+  //4end
+
   return (
     <div className="App">
-    {/*1 */}
-      <form>
-        <label htmlFor='item'>New Item</label>
-        <input type = "text" id = 'item'/>
-        <button className='btn'>click</button>
+      {/*1 */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="item">New Item</label>
+         {/*3  value and onChange*/}
+        <input  value ={newItem} onChange = {e=>setnewItem(e.target.value)}type="text" id="item" />
+         {/*3 end */}
+        <button className="btn">click</button>
       </form>
       <h1>Todo list</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button>Delete</button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button>Delete</button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 3
+          </label>
+          <button>Delete</button>
+        </li>
+
+      </ul>
 
       {/*1 end */}
-     
     </div>
   );
 }
