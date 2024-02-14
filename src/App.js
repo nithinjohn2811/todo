a45ee62e86e8aed727dc50a44a5d3e37428bb655
Nithin John Cherian <Nithinjohn2811"gmail.com>
2024-02-14 09:20:41 +0000

    created form and heading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    {/*1 */}
+      <form>
+        <label htmlFor='item'>New Item</label>
+        <input type = "text" id = 'item'/>
+        <button className='btn'>click</button>
+      </form>
+      <h1>Todo list</h1>
+
+      {/*1 end */}
      
     </div>
   );
